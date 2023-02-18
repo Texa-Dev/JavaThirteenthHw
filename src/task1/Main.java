14c501690c88591e9c3f255783b48e4c5a655c14
Texa-Dev <artemtroc26@gmail.com>
2023-02-18 22:25:05 +0200

    code clean

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Resources resources = new Resources();
         Random random = new Random();
         CountDownLatch latch = new CountDownLatch(3);
-        int countMT = 0;
+        int countMT = 0;  //число управляющих потоков
 
         do {
             Thread thread = threadMaker(random, resources, latch);
@@ -31,82 +31,4 @@ public class Main {
 
     }
 
-
-  /*
-    import java.util.concurrent.CountDownLatch;
-
-    public class Main {
-
-        public static void main(String[] args) throws InterruptedException {
-            CountDownLatch latch = new CountDownLatch(3);
-
-            // создание "числовых" потоков
-            for (int i = 0; i < 3; i++) {
-                new NumericThread(latch).start();
-            }
-
-            // создание "управляющих" потоков
-            for (int i = 0; i < 2; i++) {
-                new ControlThread(latch).start();
-            }
-
-            // ждем, пока все потоки завершат работу
-            Thread.sleep(5000);
-        }
-    }
-
-    class NumericThread extends Thread {
-        private CountDownLatch latch;
-
-        public NumericThread(CountDownLatch latch) {
-            this.latch = latch;
-        }
-
-        public void run() {
-            // увеличиваем значение общего ресурса на 1
-            SharedResource.increment();
-            System.out.println("Numeric thread incremented value to " + SharedResource.getValue());
-
-            // блокируемся, пока не получим сигнал от "управляющего" потока
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    class ControlThread extends Thread {
-        private CountDownLatch latch;
-
-        public ControlThread(CountDownLatch latch) {
-            this.latch = latch;
-        }
-
-        public void run() {
-            // отправляем сигнал "числовым" потокам
-            latch.countDown();
-            System.out.println("Control thread sent signal");
-
-            // ждем 1 секунду, чтобы дать "числовым" потокам время на выполнение
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    class SharedResource {
-        private static int value = 0;
-
-        public static void increment() {
-            value++;
-        }
-
-        public static int getValue() {
-            return value;
-        }
-    }
-*/
 }
